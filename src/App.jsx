9922fba5f4e6d9f566b392185730ff99a53d65d1
siddhesh2017 <siddheshjagtap2017@gmail.com>
2025-02-27 16:03:35 +0530

    Developed login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 import Sidebar from './assets/components/dashboard/SideBar'
 import MainContent from './assets/components/dashboard/MainContent'
 import { useState } from'react'
+import Footer from './assets/components/dashboard/Footer'
+
 
 const App = () => {
   const [isOpen, setIsOpen] = useState(true);
 
   return (
       <>
-        <div className="h-screen flex bg-blue-400 pr-3 py-4">
+        <div className="h-screen flex bg-blue-400 pr-3 pt-4 pb-8">
           <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
           <MainContent />
-      </div>
+        </div>
+        <Footer/>
       </>
   )
 }
---
+++ src/assets/components/Homepage/HomeContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomePageBanner from '../../../../public/Banner.jpeg'
+
+const HomeContent = () => {
+  return (
+    
+        <img src={HomePageBanner} className='w-full h-full object-center' alt="Home page banner" />
+    
+  )
+}
+
+export default HomeContent
