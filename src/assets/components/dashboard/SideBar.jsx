9275678066b4432b447544614f5f90f25649c0e5
siddhesh2017 <siddheshjagtap2017@gmail.com>
2025-02-28 14:50:06 +0530

    Added Sub Dropdown Menu

--- src/assets/components/dashboard/SideBar.jsx
+++ src/assets/components/dashboard/SideBar.jsx
@@ -1,119 +1,113 @@
-import { Box, ChevronDown, Home, Menu, FileSpreadsheet, User, Package, X, ShieldUser, ClipboardMinus, GraduationCap, Tally5, FileSearch2, KeyRound } from 'lucide-react';
+import { Box, ChevronDown, Home, Menu, FileSpreadsheet, User, Package, X, ShieldUser, ClipboardMinus, GraduationCap, Tally5, FileSearch2, KeyRound, QrCode } from 'lucide-react';
 import { useState } from 'react';
 import CLI_LOGO from '../../../../public/clinovations logo.jpeg'
 import { useNavigate } from 'react-router-dom';
 
 const Sidebar = ({ isOpen, setIsOpen }) => {
   const [activeDropdown, setActiveDropdown] = useState('');
+  const [activeSubDropdown, setActiveSubDropdown] = useState('');
   const navigateTo = useNavigate();
+
   const navItems = [
-    { title: 'Patient ', 
+    { 
+      title: 'Patient', 
       icon: Home, 
       hasDropdown: true,
-      dropdownItems: ['OPD', 'IPD',]
-     },
-    { 
-      title: 'Nursing', 
-      icon: User,
+      dropdownItems: [
+        { title: 'OPD', hasSubDropdown: true, subItems: ['Phone Appoinment', 'Phone Appoinment List', 'Registeration', 'Queue Management', 'Medical Records', 'OPD Bills', 'OPD Settlement', 'Bill Refund', 'View OPD Bills and Receipts', 'Followup List', 'Visitor Management', 'Courier Management', 'Vaccination'] },
+        { title: 'IPD', hasSubDropdown: true, subItems: ['Admission', 'Patient Recived', 'IPD Advanced', 'Add Services (Bill)', 'Discharge', 'Refund', 'Bed Transfer', 'View Pathelogy Report', 'View IPD Bills and Receipts', 'IPD Settlement', 'Bed View', 'Patient Display', 'Bed Release', 'Due', 'IPD Smart Access', 'Relative Bed Allocation', 'Relative BedDeAllocation'] }
+      ]
     },
+    { title: 'Nursing', icon: User },
     { 
       title: 'OT Details', 
       icon: FileSpreadsheet,
       hasDropdown: true,
-      dropdownItems: ['Inbox', 'Sent', 'Drafts', 'Archived']
+      dropdownItems: [
+        { title: 'Inbox', hasSubDropdown: false },
+        { title: 'Sent', hasSubDropdown: false },
+        { title: 'Drafts', hasSubDropdown: false },
+        { title: 'Archived', hasSubDropdown: false }
+      ]
     },
-    { title: 'Investigation', icon: FileSearch2, hasDropdown: false },
-    {
+    { title: 'Investigation', icon: FileSearch2 },
+    { 
       title: 'Inventory',
       icon: Package,
       hasDropdown: true,
-      dropdownItems: ['Preferences', 'Security', 'Notifications']
+      dropdownItems: [
+        { title: 'Preferences', hasSubDropdown: false },
+        { title: 'Security', hasSubDropdown: false },
+        { title: 'Notifications', hasSubDropdown: false }
+      ]
     },
-    { title: 'MRD', icon: Box, hasDropdown: false },
-    { title: 'NABH', icon: Box, hasDropdown: false },
-    { title: 'Administration', icon: ShieldUser, hasDropdown: false },
-    { title: 'Tally Interface', icon: Tally5, hasDropdown: false },
-    { title: 'Masters', icon: GraduationCap, hasDropdown: false },
-    { title: 'Reports', icon: ClipboardMinus, hasDropdown: false },
-    { title: 'Login', icon: KeyRound, hasDropdown: false },
-
+    { title: 'MRD', icon: Box },
+    { title: 'NABH', icon: Box },
+    { title: 'Administration', icon: ShieldUser },
+    { title: 'Tally Interface', icon: Tally5 },
+    { title: 'Masters', icon: GraduationCap },
+    { title: 'Reports', icon: ClipboardMinus },
+    { title: 'Login', icon: KeyRound }
   ];
 
   return (
-
-    // SideBar Outer div
-
-    <div 
-      className={`bg-white   scrollbar-hidden text-black transition-all duration-300 ease-in-out text-sm border-2 rounded-lg ml-4 border-[rgba(0,0,0,0.09)] 
-        ${isOpen ? 'w-88 xs:overflow-hidden' : 'w-17'} `}
-    >
-
-      {/* SideBar Header */}
-
-      <div className={`py-2.5 px-4 w-auto flex justify-between ${isOpen ? 'absolute z-10 bg-white ' : 'bg-transparent'} justify-around items-center `}>
-        <img src={CLI_LOGO} className={`w-15 h-15 transition-all duration-300  ${isOpen ? '' : 'invisible hidden '}`}/>
+    <div className={`bg-white scrollbar-hidden text-black transition-all duration-300 ease-in-out text-sm border-2 rounded-lg ml-4 border-[rgba(0,0,0,0.09)] ${isOpen ? 'w-88 xs:overflow-hidden' : 'w-17'}`}>
+  {/* SideBar Header */}
+      <div className={`py-2.5 px-5 flex justify-between ${isOpen ? 'absolute z-10 bg-white ' : 'bg-transparent'} justify-around items-center`}>
+        <img src={CLI_LOGO} className={`w-15 h-15 transition-all duration-300 ${isOpen ? '' : 'invisible hidden'}`} />
         <div>
-          <h1 className={` font-bold overflow-hidden transition-all duration-300 text-xl text-nowrap text-gray-800
-            ${isOpen ? 'opacity-100' : 'opacity-0 hidden'}`}>
-            Clinovations 
-          </h1>
-          <h1 className={` -mt-2 font-bold overflow-hidden transition-all duration-300 text-xl text-nowrap text-gray-800
-            ${isOpen ? 'opacity-100' : 'opacity-0 hidden'}`}>
-            Systems
-          </h1>
+          <h1 className={`font-bold transition-all duration-300 text-xl text-gray-800 ${isOpen ? 'opacity-100' : 'opacity-0 hidden'}`}>Clinovations </h1>
+          <h1 className={`font-bold transition-all duration-300 text-xl text-gray-800 ${isOpen ? 'opacity-100' : 'opacity-0 hidden'}`}> Systems</h1>
         </div>
-        <button 
-          onClick={() => setIsOpen(!isOpen)}
-          className={`hover:bg-blue-100 p-2 transition-all duration-300 ${isOpen? 'ml-20' : ''} rounded-lg bg-blue-200`}
-        >
-          {isOpen ? <Menu size={20} strokeWidth={1.5} /> : <Menu size={20} strokeWidth={1.5}  />}
+        <button onClick={() => setIsOpen(!isOpen)} className={`hover:bg-blue-100 p-2 transition-all duration-300 ${isOpen ? 'ml-20' : ''} rounded-lg bg-blue-200`}>
+          <Menu size={20} strokeWidth={1.5} />
         </button>
       </div>
 
-      <nav className={`${isOpen? 'mt-20' : ''} relative `}>
-        
-        {/* SideBar Options */}
-        
+  {/* DropDown Menu */}
+      <nav className={`${isOpen ? 'mt-20' : ''} relative`}>
         {navItems.map((item) => (
           <div key={item.title}>
             <div 
-              className={`${item.title === 'Login'? 'bg-blue-500 text-white hover:bg-blue-600 h-16 text-xl font-bold' : ''} px-4 py-3 hover:bg-blue-200 rounded-lg m-2 cursor-pointer flex items-center justify-between`}
+              className={`px-4 py-3 hover:bg-blue-200 rounded-lg m-2 cursor-pointer flex items-center justify-between ${item.title === 'Login' ? 'bg-blue-500 text-white hover:bg-blue-600 h-16 text-xl font-bold' : ''}`} 
               onClick={() => {
                 if (item.title === 'Login') {
                   navigateTo('/login');
-                }
-                else{
-                  item.hasDropdown && isOpen && setActiveDropdown(activeDropdown === item.title ? '' : item.title)
+                } else {
+                  item.hasDropdown && isOpen && setActiveDropdown(activeDropdown === item.title ? '' : item.title);
                 }
               }}
             >
-              <div className="flex items-center">
-                <item.icon size={`${item.title === 'Login' ? 23 : 20}`} strokeWidth={1.5} color={`${item.title === 'Login' ? 'white' : '#000'}`} />
-                <span className={`ml-4 whitespace-nowrap overflow-hidden transition-all duration-300
-                  ${isOpen ? 'w-32 opacity-100' : 'w-0 opacity-0'}`}>
-                  {item.title}
-                </span>
+              <div className="flex items-center ">
+                <item.icon size={20} strokeWidth={1.5} color={item.title === 'Login' ? 'white' : '#000'} />
+                <span className={`ml-4 transition-all duration-300 ${isOpen ? 'opacity-100' : 'opacity-0 hidden'}`}>{item.title}</span>
               </div>
               {item.hasDropdown && isOpen && (
-                <ChevronDown 
-                  size={16} 
-                  strokeWidth={1.5}
-                  className={`transition-transform duration-200 
-                    ${activeDropdown === item.title ? 'rotate-180' : ''}`}
-                />
+                <ChevronDown size={16} strokeWidth={1.5} className={`transition-transform duration-200 ${activeDropdown === item.title ? 'rotate-180' : ''}`} />
               )}
             </div>
-            
-              {/* SideBar Dropdown Options */}
-
             {item.hasDropdown && isOpen && activeDropdown === item.title && (
-              <div className="bg-blue-200 overflow-hidden rounded-lg mx-2 transition-all duration-800">
+              
+  // DropDown Options
+              <div className="bg-gray-100 ml-2 pl-4 py-2 pr-1 border-l-1 transition-all duration-800">
                 {item.dropdownItems.map((dropdownItem) => (
-                  <div
-                    key={dropdownItem}
-                    className="px-11 py-2 hover:bg-blue-100 cursor-pointer text-sm"
-                  >
-                    {dropdownItem}
+                  <div key={dropdownItem.title}>
+                    <div 
+                      className="px-11 py-2 rounded-lg hover:bg-blue-100 cursor-pointer flex justify-between items-center"
+                      onClick={() => dropdownItem.hasSubDropdown && setActiveSubDropdown(activeSubDropdown === dropdownItem.title ? '' : dropdownItem.title)}
+                    >
+                      {dropdownItem.title}
+                      {dropdownItem.hasSubDropdown && <ChevronDown size={14} strokeWidth={1.5} className={`transition-transform duration-200 ${activeSubDropdown === dropdownItem.title ? 'rotate-180' : ''}`} />}
+                    </div>
+                    {dropdownItem.hasSubDropdown && activeSubDropdown === dropdownItem.title && (
+                      
+  // SubDropDown Options
+                      <div className="my-2 p-2 border-l-1 bg-gray-200  transition-all duration-800">
+                        {dropdownItem.subItems.map((subItem) => (
+                          <div key={subItem} className="px-14 py-2 rounded-lg  hover:bg-blue-200 cursor-pointer">{subItem}</div>
+                        ))}
+                      </div>
+                    )}
                   </div>
                 ))}
               </div>
--- src/assets/components/dashboard/Temp.jsx
+++ src/assets/components/dashboard/Temp.jsx
@@ -2,40 +2,7 @@ import React from 'react'
 
 const Temp = () => {
   return (
-    <div>
-        
-
-        <div class="inline-flex">
-            <button type="button" class="relative py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-s-md border border-gray-200 bg-white text-gray-800 hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800 dark:focus:bg-neutral-800">
-                Split dropleft
-            </button>
-            <div class="hs-dropdown relative [--placement:bottom-left] inline-flex">
-                <button id="hs-split-dropleft" type="button" class="hs-dropdown-toggle relative -ms-[.3125rem] py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-e-md border border-gray-200 bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-none focus:bg-gray-200 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white dark:focus:bg-neutral-700 dark:focus:text-white" aria-haspopup="menu" aria-expanded="false" aria-label="Dropdown">
-                <span class="sr-only">Toggle Dropdown</span>
-                <svg class="shrink-0 size-4 text-gray-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                    <path d="m9 18 6-6-6-6"></path>
-                </svg>
-                </button>
-
-                <div class="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white shadow-md rounded-lg mt-2 dark:bg-neutral-800 dark:border dark:border-neutral-700" role="menu" aria-orientation="vertical" aria-labelledby="hs-split-dropleft">
-                <div class="p-1 space-y-0.5">
-                    <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700" href="#">
-                    Newsletter
-                    </a>
-                    <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700" href="#">
-                    Purchases
-                    </a>
-                    <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700" href="#">
-                    Downloads
-                    </a>
-                    <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700" href="#">
-                    Team Account
-                    </a>
-                </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <div>Temp</div>
   )
 }
 
